1644e630885df305e77165652712ac8e137c505c
mairaslimas <mairas.limas@gmail.com>
2023-08-23 18:43:20 -0300

    Endpoint Create para Tecnico

--- src/main/java/com/mairaslimas/helpdesk/services/TecnicoService.java
+++ src/main/java/com/mairaslimas/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mairaslimas.helpdesk.domain.Tecnico;
+import com.mairaslimas.helpdesk.domain.dtos.TecnicoDTO;
 import com.mairaslimas.helpdesk.repositories.TecnicoRepository;
 import com.mairaslimas.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
