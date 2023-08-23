a08c05f5759e49943a1b012439279bbf56c8c7ac
mairaslimas <mairas.limas@gmail.com>
2023-08-23 13:14:57 -0300

    Perfil de teste criado

---
+++ src/main/java/com/mairaslimas/helpdesk/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.mairaslimas.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mairaslimas.helpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		dbService.instanciaDB();
+	}
+}
