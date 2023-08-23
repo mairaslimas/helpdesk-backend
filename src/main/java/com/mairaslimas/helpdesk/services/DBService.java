27f7d1ccfc39cc6659af9bc11c4d9c9294c7e239
mairaslimas <mairas.limas@gmail.com>
2023-08-23 17:12:19 -0300

    Endpoint findById tecnico

--- src/main/java/com/mairaslimas/helpdesk/services/DBService.java
+++ src/main/java/com/mairaslimas/helpdesk/services/DBService.java
@@ -12,29 +12,40 @@ import com.mairaslimas.helpdesk.domain.enums.Perfil;
 import com.mairaslimas.helpdesk.domain.enums.Prioridade;
 import com.mairaslimas.helpdesk.domain.enums.Status;
 import com.mairaslimas.helpdesk.repositories.ChamadoRepository;
-import com.mairaslimas.helpdesk.repositories.ClienteRepository;
-import com.mairaslimas.helpdesk.repositories.TecnicoRepository;
+import com.mairaslimas.helpdesk.repositories.PessoaRepository;
 
 @Service
 public class DBService {
 	@Autowired
-	private TecnicoRepository tecnicoRepository;
+	private PessoaRepository pessoaRepository;
 	
-	@Autowired
-	private ClienteRepository clienteRepository;
 	
 	@Autowired
 	private ChamadoRepository chamadoRepository;
 	public void instanciaDB() {
-		Tecnico tec1 = new Tecnico(null,"Valdir Cezar","92306813000","valdir@mail.com","123");
+		Tecnico tec1 = new Tecnico(null,"Valdir Cezar","923.068.130-00","valdir@mail.com","123");
 		tec1.addPerfil(Perfil.ADMIN);
+		Tecnico tec2 = new Tecnico(null,"Richard Stallman","997.982.740-83","stallman@mail.com","123");
+		Tecnico tec3 = new Tecnico(null,"Claude Elwood Shannon","870.521.230-88","shannon@mail.com","123");
+		Tecnico tec4 = new Tecnico(null,"Tim Berners-Lee","030.777.020-61","lee@mail.com","123");
+		Tecnico tec5 = new Tecnico(null,"Linus Torvalds","630.727.630-46","linus@mail.com","123");
 		
-		Cliente cli1 = new Cliente(null,"Linus Torvalds","64474635060","torvalds@mail.com","123");
 		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
 		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
+		Cliente cli1 = new Cliente(null,"Albert Einstein","993.088.820-90","einstein@mail.com","123");
+		Cliente cli2 = new Cliente(null,"Marie Curie","010.517.530-74","curie@mail.com","123");
+		Cliente cli3 = new Cliente(null,"Charles Darwin","769.329.390-61","darwin@mail.com","123");
+		Cliente cli4 = new Cliente(null,"Stephen Hawking","300.172.490-07","hawking@mail.com","123");
+		Cliente cli5 = new Cliente(null,"Max Planck","445.439.800-34","planck@mail.com","123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Teste Chamado 1", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 2", "Teste Chamado 2", tec1, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 3", "Teste Chamado 3", tec2, cli3);
+		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 4", "Teste Chamado 4", tec3, cli3);
+		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 5", "Teste Chamado 5", tec2, cli1);
+		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 6", "Teste Chamado 6", tec1, cli5);
+		
+		pessoaRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5, cli1, cli2, cli3, cli4, cli5));
+		chamadoRepository.saveAll(Arrays.asList(c1,c2, c3, c4, c5, c6));
 	}
 }
